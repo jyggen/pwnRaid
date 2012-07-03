af5440d11ebdea47792418c90a50c75f17e3a697
Jonas Stendahl <jonas.stendahl@gmail.com>
2012-07-03 02:06:35 +0200

    Remove 'em!

--- pwnraid/core/logs/2012/06/27.php
+++
@@ -1,6 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-Warning - 2012-06-27 21:08:57 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-Warning - 2012-06-27 21:08:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-Warning - 2012-06-27 21:08:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-Warning - 2012-06-27 21:08:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
